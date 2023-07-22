57d291573cae65a0dabc3bf3941087d2c867896f
Kieran Hulsman <kghulsma@uwaterloo.ca>
2023-07-22 14:50:07 -0400

    skeleton created

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 # kieran hulsman
 # wordle solver
+
+# global variables (filters)
+gray = set()
+green = ['','','','','']
+yellow_indexed = [[],[],[],[],[]]
+yellow_set = set()
+
+def get_guess () -> str:
+    pass
+
+def get_feedback (guess: str) -> str:
+    pass
+
+def update_filter (feedback: str) -> None:
+    pass
+
+def main ():
+    for i in range(6):
+        guess = get_guess()
+        feedback = get_feedback(guess)
+        update_filter(feedback)
+
+if __name__=="__main__":
+    main()
